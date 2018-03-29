237a3026c5aafd8f2722660bd4cff57abed61a3e
louiemain <louielcxy@live.cn>
2018-03-30 00:18:06 +0800

    完成倒计时功能-每分钟刷新1次

--- app/src/main/java/com/example/louiemain/pripathology/pager/MinePager.java
+++ app/src/main/java/com/example/louiemain/pripathology/pager/MinePager.java
@@ -15,6 +15,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.example.louiemain.pripathology.R;
 import com.example.louiemain.pripathology.activity.TopicRecordActivity;
@@ -31,6 +32,10 @@ import java.net.HttpURLConnection;
 import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.UnknownHostException;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * @Pragram: PriPathology
@@ -41,6 +46,7 @@ import java.net.UnknownHostException;
  **/
 public class MinePager extends BasePager {
 
+    private static final int COUNT_DOWN = 4;
     private CardView cv_download_data;
     private CardView cv_upload_data;
 
@@ -60,9 +66,17 @@ public class MinePager extends BasePager {
     private boolean ifInterrupt = false;
     private CardView cv_order_record;
     private CardView cv_random_record;
+    private TextView tv_count_down;
 
     public MinePager(Context context) {
         super(context);
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                handler.sendEmptyMessage(COUNT_DOWN);
+            }
+        }.start();
     }
 
     @Override
@@ -82,7 +96,36 @@ public class MinePager extends BasePager {
         cv_order_record.setOnClickListener(new MyOnClickListener());
         cv_random_record.setOnClickListener(new MyOnClickListener());
 
+        tv_count_down = (TextView) view.findViewById(R.id.tv_count_down);
+
         return view;
+    }
+
+    /**
+     * 计算倒计时
+     */
+    private String calcCountDown() {
+        // 考试时间为5.26
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT+8"));
+        // 当前时间戳
+        long currentTime = calendar.getTimeInMillis();
+        calendar.set(Calendar.MONTH, 4);
+        calendar.set(Calendar.DATE, 26);
+        calendar.set(Calendar.HOUR, 9);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        // 考试时间戳
+        long examTime = calendar.getTimeInMillis();
+        // 倒计时
+        long rang = examTime - currentTime;
+        long nd = 1000 * 24 * 60 * 60;  // 一天
+        long nh = 1000 * 60 * 60;
+        long nm = 1000 * 60;
+
+        long day = rang / nd;
+        long hour = rang % nd / nh;
+        long min = rang % nd % nh / nm;
+        return day + "天" + hour + "小时" + min + "分";
     }
 
     private class MyOnClickListener implements View.OnClickListener {
@@ -98,12 +141,13 @@ public class MinePager extends BasePager {
                     break;
                 case R.id.cv_order_record:
                     intent.putExtra("tag", "order");
+                    context.startActivity(intent);
                     break;
                 case R.id.cv_random_record:
                     intent.putExtra("tag", "random");
+                    context.startActivity(intent);
                     break;
             }
-            context.startActivity(intent);
         }
     }
 
@@ -135,8 +179,8 @@ public class MinePager extends BasePager {
                     }
                     try {
                         String result = "";
-                        url = new URL("http://192.168.110.94/blcj/get/" + i);
-//                        url = new URL("http://192.168.1.102/blcj/get/" + i);
+//                        url = new URL("http://192.168.110.94/blcj/get/" + i);
+                        url = new URL("http://192.168.1.102/blcj/get/" + i);
                         conn = (HttpURLConnection) url.openConnection();
                         conn.setRequestMethod("GET");
                         conn.setConnectTimeout(3000);
@@ -190,18 +234,30 @@ public class MinePager extends BasePager {
             switch (msg.what) {
                 case LINK_NETWORK_FAIL:
                     Toast.makeText(context, "连接服务器失败，请稍后重试。", Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+
                     break;
                 case UPDATE_SUCCESS:
                     Toast.makeText(context, "数据更新成功。", Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+
                     break;
                 case UPDATE_FAILURE:
                     Toast.makeText(context, "数据更新失败，线程被终止。请退出程序后重试。", Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+
                     break;
                 case SOCKET_TIMEOUT:
                     Toast.makeText(context, "服务器连接超时，请稍后重试。", Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+
+                    break;
+                case COUNT_DOWN:
+                    tv_count_down.setText("距离考试" + calcCountDown());
+                    removeMessages(COUNT_DOWN);
+                    sendEmptyMessageDelayed(COUNT_DOWN, 1000 * 60);
                     break;
             }
-            progressDialog.dismiss();
         }
     };
 
